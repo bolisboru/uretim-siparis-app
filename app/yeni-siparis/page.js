1397f041d966623577853b478374e872ed0a213a
bolisboru <bolisplastikpazarlama@gmail.com>
2026-07-28 00:27:44 +0300

    Create page.js

---
+++ app/yeni-siparis/page.js
@@ -0,0 +1,10 @@
+'use client'
+export default function YeniSiparis() {
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold">Random Firma</h1>
+      <p>Tel: 0555 000 00 00 | Adres: Random Mah. No:1 Bursa</p>
+      <button className="bg-green-500 text-white p-2 rounded">PDF İndir</button>
+    </main>
+  )
+}
